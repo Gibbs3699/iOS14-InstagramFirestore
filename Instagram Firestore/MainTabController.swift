8e9c12a3f92dbab2e5101eb61acaa28908a8daad
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-03-18 22:25:07 +0700

    Configure Project

---
+++ Instagram Firestore/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  Instagram Firestore
+//
+//  Created by TheGIZzz on 18/3/2565 BE.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+         super.viewDidLoad()
+        
+        
+        view.backgroundColor = .green
+    }
+}
