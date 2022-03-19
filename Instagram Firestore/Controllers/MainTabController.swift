df8542384d7b63b9b1fd282cc86e3405af1b08f2
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-03-19 19:18:57 +0700

    Setting Up CollectionView

--- Instagram Firestore/Controllers/MainTabController.swift
+++ Instagram Firestore/Controllers/MainTabController.swift
@@ -9,23 +9,28 @@ import UIKit
 
 class MainTabController: UITabBarController {
     
-    //MARK: -Lifecycle
+    //MARK: - Lifecycle
     //get calls when the view loads in memory on the application
     override func viewDidLoad() {
          super.viewDidLoad()
-          configureViewControllers()
+        configureViewControllers()
     
     }
     
-    //MARK: -Helpers
+    //MARK: - Helpers
     // store instance of viewcontrollers
     func configureViewControllers() {
         view.backgroundColor = .white
         
-        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController())
+        let layout = UICollectionViewFlowLayout()
+        let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
+        
         let search = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"), rootViewController: SearchController())
+        
         let imageSelector = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"), rootViewController: ImageSelectorController())
+        
         let notification = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"), rootViewController: NotificationController())
+        
         let profile = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"), selectedImage: #imageLiteral(resourceName: "profile_selected"), rootViewController: ProfileController())
         
         viewControllers = [feed , search, imageSelector, notification, profile]
@@ -38,6 +43,15 @@ class MainTabController: UITabBarController {
         nav.tabBarItem.image = unselectedImage
         nav.tabBarItem.selectedImage = selectedImage
         nav.navigationBar.tintColor = .black
+        
+        if #available(iOS 15.0, *) {
+            let appearance = UITabBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = .white
+            tabBar.standardAppearance = appearance
+            tabBar.scrollEdgeAppearance = tabBar.standardAppearance
+        }
+        
         return nav
     }
 }
